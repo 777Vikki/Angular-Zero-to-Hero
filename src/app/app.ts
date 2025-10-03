3bcf1d2ba6c5a25218382bf78d71065f00024336
Vivek <vkmm777@gmail.com>
2025-10-03 17:17:50 +0530

    create pipe

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UppercasePipe } from './shared/pipes/uppercase-pipe';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UppercasePipe],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
