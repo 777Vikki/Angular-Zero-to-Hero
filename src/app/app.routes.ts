efd612f148bff5fef779ecb97b4d42496eb93886
Vivek <vkmm777@gmail.com>
2025-10-03 20:09:54 +0530

    add reactive form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: 'reactive-form-single-control', loadComponent: () => import('./reactive-forms/reactive-form-single-control/reactive-form-single-control').then(m => m.ReactiveFormSingleControl) },
-
+    { path: 'reactive-form', loadComponent: () => import('./reactive-forms/reactive-form/reactive-form').then(m => m.ReactiveForm) },
+    { path: 'reactive-form=validation', loadComponent: () => import('./reactive-forms/reactive-form-validation/reactive-form-validation').then(m => m.ReactiveFormValidation) },
+    
 ];
--- src/app/reactive-forms/reactive-form/reactive-form.ts
+++ src/app/reactive-forms/reactive-form/reactive-form.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './reactive-form.html',
   styleUrl: './reactive-form.scss'
 })
 export class ReactiveForm {
+  private formBuilder = inject(FormBuilder);
+  myForm: FormGroup = this.formBuilder.group({
+    name: [''],
+    email: [''],
+    phoneNumber: ['']
+  });
 
+  onSubmit() {
+    console.log(this.myForm.value);
+  }
 }
