6702fdda2b8c14cb42434410709246e1e727afe1
Vivek <vkmm777@gmail.com>
2025-10-04 12:41:35 +0530

    add custom validator

--- src/app/reactive-forms/reactive-form-custom-validation/reactive-form-custom-validation.ts
+++ src/app/reactive-forms/reactive-form-custom-validation/reactive-form-custom-validation.ts
@@ -1,11 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form-custom-validation',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './reactive-form-custom-validation.html',
   styleUrl: './reactive-form-custom-validation.scss'
 })
 export class ReactiveFormCustomValidation {
+  formBuilder = inject(FormBuilder);
 
+  myForm: FormGroup = this.formBuilder.group({
+    userName: ['', [Validators.required, this.noSpaceValidator()]],
+    password: ['', [Validators.required, this.passwordStrengthValidator()]],
+  },
+    { validators: this.formErrorValidator() }
+  );
+
+  noSpaceValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const hasSpace = (control.value || '').includes(' ');
+      return hasSpace ? { noSpace: true } : null;
+    };
+  }
+
+  passwordStrengthValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value = control.value || '';
+
+      // Must contain letters, numbers, and at least one special character
+      const pattern = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/;
+
+      return pattern.test(value) ? null : { passwordStrength: true };
+    };
+  }
+
+  formErrorValidator(): ValidatorFn {
+    return (group: AbstractControl): ValidationErrors | null => {
+      const userName = group.get('userName')?.value || '';
+      const password = group.get('password')?.value || '';
+
+      if (!userName || !password) {
+        return { formInvalid: 'Please fill all fields.' };
+      }
+
+      if (userName.includes(' ')) {
+        return { formInvalid: 'Username should not contain spaces.' };
+      }
+
+      const passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&]).+$/;
+      if (!passwordPattern.test(password)) {
+        return { formInvalid: 'Password must be alphanumeric with a special character.' };
+      }
+      return null;
+    };
+  }
+
+  onSubmit() {
+    console.log(this.myForm);
+  }
 }
