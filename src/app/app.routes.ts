3208652df582f26a91796cd80005f5a588d6afc3
Vivek <vkmm777@gmail.com>
2025-11-07 12:48:24 +0530

    clean code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,3 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-    { path: 'reactive-form-single-control', loadComponent: () => import('./reactive-forms/reactive-form-single-control/reactive-form-single-control').then(m => m.ReactiveFormSingleControl) },
-    { path: 'reactive-form', loadComponent: () => import('./reactive-forms/reactive-form/reactive-form').then(m => m.ReactiveForm) },
-    { path: 'reactive-form-validation', loadComponent: () => import('./reactive-forms/reactive-form-validation/reactive-form-validation').then(m => m.ReactiveFormValidation) },
-    { path: 'reactive-form-custom-validation', loadComponent: () => import('./reactive-forms/reactive-form-custom-validation/reactive-form-custom-validation').then(m => m.ReactiveFormCustomValidation) },
-    
-];
+export const routes: Routes = [];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('Angular-Zero-to-Hero');
 }
